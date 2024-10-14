70f7c41a984887b28116f72b98112e143a6c26ba
lautaroballesteros0926 <lautaroballesteros26@gmail.com>
2024-10-14 14:16:17 -0500

    Agregando la clase snake/Agregando deteccion de colisiones con obstaculos

--- src/game.py
+++ src/game.py
@@ -1,6 +1,7 @@
 import pygame
 from tablero import Tablero
 from obstaculos import Obstaculos
+from snake import Snake
 # Colores básicos
 BLACK = (0, 0, 0)
 
@@ -14,11 +15,27 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Snake Mejorado")
         self.clock = pygame.time.Clock()
+
         self.tablero = Tablero(WIDTH, HEIGHT, CELL_SIZE)
+        self.snake = Snake()
         self.obstaculos = Obstaculos(self.tablero)
 
         self.running = True
         self.score = 0
+
+    def check_collisions(self):
+        # Colisión con los bordes del tablero
+        head_x, head_y = self.snake.body[0]
+        if not (0 <= head_x < self.tablero.columns and 0 <= head_y < self.tablero.rows):
+            self.running = False
+
+        # Colisión con el propio cuerpo
+        if len(self.snake.body) != len(set(self.snake.body)):
+            self.running = False
+
+        # Colisión con obstáculos
+        if (head_x, head_y) in self.obstaculos.obstacles:
+            self.running = False
 
     def handle_input(self):
         for event in pygame.event.get():
@@ -26,26 +43,27 @@ class Game:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    # definir algun movimiento
-                    pass
+                    self.snake.set_direction(0, -1)
                 elif event.key == pygame.K_DOWN:
-                    # definir algun movimiento
-                    pass
+                    self.snake.set_direction(0, 1)
                 elif event.key == pygame.K_LEFT:
-                    # definir algun movimiento
-                    pass
+                    self.snake.set_direction(-1, 0)
                 elif event.key == pygame.K_RIGHT:
-                    # definir algun movimiento
-                    pass
+                    self.snake.set_direction(1, 0)
 
     def game_loop(self):
+        self.obstaculos.generar_obstaculos(5)  # Generar obstáculos
 
         while self.running:
             self.handle_input()
+            self.snake.move()
+            self.check_collisions()
 
             self.screen.fill(BLACK)
             self.tablero.draw(self.screen)
-            self.obstaculos(self.screen)
+            self.snake.draw(self.screen)
+            self.obstaculos.draw(self.screen)
+            self.power_ups.draw(self.screen)
 
             pygame.display.flip()
             self.clock.tick(10)  # 10 FPS
